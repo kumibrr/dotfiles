ff038cf30bfcadd54418535641036c11969180d5
kumibrr <isra.lora99@gmail.com>
2021-08-13 19:59:36 +0200

    Added nwg-bar installation and github cli installation

--- setup.sh
+++ setup.sh
@@ -12,8 +12,18 @@ sudo zypper refresh
 
 sudo zypper -n install -l microsoft-edge-beta code
 
+# Installing github CLI
+sudo zypper addrepo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo zypper -n --gpg-auto-import-keys ref
+sudo zypper -n install gh
+
 # Installing NVM, node
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh)"
+
+nvm install node
+
+# Installing tldr
+npm install -g tldr
 
 # Installing 1Password
 mkdir $HOME/temp
@@ -47,8 +57,12 @@ pip install pyxdg
 
 /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
-# Installing window manager
-sudo zypper -n in -t pattern sway
+# Installing sway extensions
+sudo zypper -n install make
+git clone https://github.com/nwg-piotr/nwg-bar.git $HOME/temp/ngwbar
+cd $HOME/temp/ngwbar
+sudo make install
+cd $HOME
 
 #Installing fonts
 sudo wget -O /usr/share/fonts/truetype/'MesloLGS NF Regular.ttf' https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
@@ -58,6 +72,8 @@ sudo wget -O /usr/share/fonts/truetype/'MesloLGS NF Bold Italic.ttf' https://git
 sudo wget -O $HOME/temp/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip $HOME/temp/FiraCode.zip -d $HOME/temp/FiraCode
 sudo mv $HOME/temp/FiraCode/* /usr/share/fonts/truetype/
+
+# Commenting default opensuse wallpaper for sway
 
 #Cleaning up
 sudo rm -r $HOME/temp
